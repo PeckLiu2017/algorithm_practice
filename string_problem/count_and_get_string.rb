52396651058f7a12a480265ad776f70f689acad7
peck liu <xiaoyang20170204@gmail.com>
2017-10-03 02:00:18 +0800

    get char at index

--- string_problem/count_and_get_string.rb
+++ string_problem/count_and_get_string.rb
@@ -2,36 +2,55 @@ def get_count_string(str)
   return '' if str.nil? || str.eql?('')
   chs = str.chars
   res = chs[0]
-  p "s1"
-  p "res is #{res}"
   num = 1
+  # 从索引为1的字符开始
   for i in 1..chs.size-1
-  # chs.each_with_index do |chs[i], i|
-    p "s2"
-    p "chs[i] is #{chs[i]}"
+    # 如果前后两个字母不连续
     if chs[i] != chs[i - 1]
-      p "s3"
-      p "num is #{num}"
+      # 就开始拼接字符串
       res = concat(res, num.to_s, chs[i])
       num = 1
     else
-      p "s4"
       num += 1
-      p "num is #{num}"
     end
   end
   return concat(res, num.to_s, '')
 end
 
 def concat(s1, s2, s3)
-  p s1
-  p s2
-  p s3
   s1 + '_' + s2 + (s3.eql?('') ? s3 : '_' + s3)
 end
 
+def get_char_at(str, index)
+  return 0 if str.nil? || str.eql?('')
+  chs = str.chars
+  stage = true
+  cur = 0
+  num = 0
+  sum = 0
+  chs.each_with_index do |v, i|
+    if v == '_'
+      stage = !stage
+    elsif stage
+      # 记录数字 sum 和字母 cur 组成的区域
+      sum += num
+      # 判断字母区域是否在范围内
+      # 如果在
+      # 从上一个 cur 区域中求值
+      if sum > index
+        return cur
+      end
+      num = 0
+      cur = v
+    else
+      # 记录数字
+      num = num * 10 + v.to_i - 0
+    end
+  end
+  sum + num > index ? cur : 0;
+end
 
-str = 'aaabbadddffc'
-res = get_count_string(str)
+str1 = 'aaabbadddffc'
+res = get_count_string(str1)
 p res
-# p getCharAt(res, 9)
+p get_char_at(res, 3)
