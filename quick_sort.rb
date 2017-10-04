1639b25a45969defb0a1e736c9efd678232a690a
peck liu <xiaoyang20170204@gmail.com>
2017-10-04 22:02:54 +0800

    some modify

--- quick_sort.rb
+++ quick_sort.rb
@@ -9,23 +9,22 @@
 # 两个相同的数也可能被调换位置
 # 从而破坏原来数的稳定性
 def quick_sort(arr, left, right)
-  # 非空验证
-  return if left > right
-  # 递归的层次
-  # left = 0, right = 4 时
-  # left = 0, right = 2 时
-  # left = 0, right = 1 时
-  # 不断寻找基准点
+  return if left < right
   pivot_pos = get_middle(arr, left, right)
-  # 基准点左边的
+  # 因为基准数在左边
+  # 这是最重要的原因
+  # 最后左右指针相遇时要和基准数交换
+  # 相遇的数值要比基准数小
+  # 所以right指针先移动
+  # 寻找比基准数小的数
   quick_sort(arr, left, pivot_pos - 1)
-  # 基准点右边的
   quick_sort(arr, pivot_pos + 1, right)
 end
 
 def sort(arr)
+  # 数字和空值、字符串无法比较
   return if arr.include?('') || array.size < 2
-  quick_sort(arr, 0, arr.size - 1)
+  quick_sort(arr, 0 arr.size - 1)
   arr
 end
 
@@ -33,30 +32,22 @@ def get_middle(arr, left, right)
   # 数组的第一个作为基准值
   pivot_key = arr[left]
   pivot_pointer = left
+  # 只要 left < right,两边就没有相遇
   while left < right
+    # 为了相遇,右边的指针往左边移动(相遇了会怎么样？)
     while left < right && arr[right] >= pivot_key
       right -= 1
     end
-    # 比基准值小的记录移到低端
     arr[left] = arr[right]
+    # 为了相遇,左边的指针往右边移动(相遇了会怎么样？)
     while left < right && arr[left] <= pivot_key
       left += 1
     end
-    # 比基准值大的记录移到高端
     arr[right] = arr[left]
   end
-  # 把基准值放到上一步移到右边的那个值的位置
   arr[left] = pivot_key
   return left
 end
-
-# 第二种解法
-# def quick_sort(array)
-#   return array if array.size < 2
-#   left, right = array[1..-1].partition { |y| y <= array.first }
-#   quick_sort(left) + [array.first] + quick_sort(right)
-# end
-
-arr = [5, 3, 8, 6, 4]
+arr = [5, 3, 8, 3, 4]
 # arr = [57,68,59,52,72,28,96,33,24,19]
 p sort(arr)
